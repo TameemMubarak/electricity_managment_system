a1162d066cd062c521c3c704d588315c1e836c5d
TameemMubarak <m.ansarithameem004@gmail.com>
2026-04-13 22:03:25 +0530

    Update login.java

--- src/login.java
+++ src/login.java
@@ -69,7 +69,7 @@ public class login extends JFrame implements ActionListener
 
 
     public void actionPerformed(ActionEvent ae){
-// this line causes exception so i putit inside
+// this lin
         try{
             conn c1 = new conn();
             String a  = tf1.getText();
